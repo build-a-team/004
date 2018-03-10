a00fd4519a4b405c0e19f571aca128d718298457
t1t1 <cha@hanmail2.net>
2018-03-10 22:00:11 +0900

    Add FeedPage

---
+++ src/containers/FeedPage/FeedPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import classNames from "classnames/bind";
+import styles from "./LoginPage.scss";
+const cx = classNames.bind(styles);
+
+class FeedPage extends Component {
+    state = {  }
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        );
+    }
+}
+
+export default FeedPage;
