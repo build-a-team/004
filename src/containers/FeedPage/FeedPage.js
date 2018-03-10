9d44e7cbaa292b486dc5854c1f3c520a039fedd7
t1t1 <cha@hanmail2.net>
2018-03-11 00:12:31 +0900

    Add feeds firebase

--- src/containers/FeedPage/FeedPage.js
+++ src/containers/FeedPage/FeedPage.js
@@ -15,26 +15,34 @@ class FeedPage extends Component {
             {
                 imageUrl: '"https://firebasestorage.googleapis.com/v0/b/build004-f298c.appspot.com/o/cat.jpg61a07880-246a-11e8-a78e-69fff36fa26a?alt=media&token=cd671040-944b-4f7a-b60e-c233440cc073"',
                 rates: {
-                    '1': 1,
+                    '1': -1,
                     '2': 1,
-                    '3': 1,
-                    '4': 1,
-                    '5': 1
+                    '3': -1,
+                    '4': 1
                 },
                 tags: {}
             },
             {
                 imageUrl: '"https://firebasestorage.googleapis.com/v0/b/build004-f298c.appspot.com/o/cat.jpg61a07880-246a-11e8-a78e-69fff36fa26a?alt=media&token=cd671040-944b-4f7a-b60e-c233440cc073"',
                 rates: {
-                    '1': 2,
-                    '2': 2,
-                    '3': 2,
-                    '4': 2,
-                    '5': 2
+                    '1': 1,
+                    '2': 0,
+                    '3': 1,
+                    '4': 0
                 },
                 tags: {}
             }
         ]
+    }
+
+    componentDidMount() {
+        feedsRef.on('value', snap => {
+            const feeds = [];
+            snap.forEach(shot => {
+                feeds.push({ ...shot.val(), key: shot.key });
+            });
+            // this.setState({ feeds });
+        });
     }
 
     sumRate(rates) {
