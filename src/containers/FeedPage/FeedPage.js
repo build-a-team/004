25d76b62b949b4d227820326a03b9e8b1aa50a7c
t1t1 <cha@hanmail2.net>
2018-03-10 23:50:54 +0900

    Add feeds

--- src/containers/FeedPage/FeedPage.js
+++ src/containers/FeedPage/FeedPage.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import firebase from 'config/firebase';
 import classNames from "classnames/bind";
-import styles from "./LoginPage.scss";
+import styles from "./FeedPage.scss";
 const cx = classNames.bind(styles);
 
+const rootRef = firebase.database().ref();
+const feedsRef = rootRef.child('feeds');
+const timeRef = firebase.database.ServerValue.TIMESTAMP;
+
 class FeedPage extends Component {
-    state = {  }
+    state = { 
+        feeds: [
+            {
+                imageUrl: '"https://firebasestorage.googleapis.com/v0/b/build004-f298c.appspot.com/o/cat.jpg61a07880-246a-11e8-a78e-69fff36fa26a?alt=media&token=cd671040-944b-4f7a-b60e-c233440cc073"',
+                rates: {
+                    '1': 1,
+                    '2': 1,
+                    '3': 1,
+                    '4': 1,
+                    '5': 1
+                },
+                tags: {}
+            },
+            {
+                imageUrl: '"https://firebasestorage.googleapis.com/v0/b/build004-f298c.appspot.com/o/cat.jpg61a07880-246a-11e8-a78e-69fff36fa26a?alt=media&token=cd671040-944b-4f7a-b60e-c233440cc073"',
+                rates: {
+                    '1': 2,
+                    '2': 2,
+                    '3': 2,
+                    '4': 2,
+                    '5': 2
+                },
+                tags: {}
+            }
+        ]
+    }
+
+    sumRate(rates) {
+        return _.reduce(rates, function(result, value, key) {
+            return result + _.parseInt(value);
+        }, 0)
+    }
+
     render() {
         return (
             <div>
-                hello
+                {
+                    _.map(this.state.feeds, (feed) => {
+                        return <div>
+                            <div>
+                                <img src={feed.imageUrl} alt="" />
+                            </div>
+                            <div>{this.sumRate(feed.rates)}</div>
+                        </div>
+                    })
+                }
             </div>
         );
     }
