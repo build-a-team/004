aed776a63fb643274e301bb66d9a21f9c95f9cef
LimHoSung <wonder789@naver.com>
2018-03-10 22:47:50 +0900

    email Login기능 테스트완료

--- src/containers/LoginPage/LoginPage.js
+++ src/containers/LoginPage/LoginPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import firebase from "config/firebase";
 import classNames from "classnames/bind";
 import styles from "./LoginPage.scss";
 const cx = classNames.bind(styles);
@@ -18,22 +19,58 @@ class LoginPage extends Component {
         });
     };
 
+    handleLogin = ev => {
+        let email = this.state.email,
+            password = this.state.password;
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(response => {
+                alert("로그인 성공");
+            })
+            .catch(error => {
+                // Handle Errors here.
+
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // ...
+                if (errorCode === "auth/wrong-password") {
+                    alert("Wrong password.");
+                } else {
+                    alert(errorMessage);
+                }
+            });
+
+        ev.preventDefault();
+    };
+
     render() {
         return (
             <div className={cx("login-page")}>
                 <h1>Login Page</h1>
                 <div className={cx("form")}>
-                    <div className="form-group">
-                        <label>이메일</label>
-                        <input type="text" name="email" />
-                    </div>
-                    <div className="form-group">
-                        <label>비밀번호</label>
-                        <input type="password" name="password" />
-                    </div>
-                    <div className="form-group">
-                        <button>로그인</button>
-                    </div>
+                    <form onSubmit={this.handleLogin}>
+                        <div className="form-group">
+                            <label>이메일</label>
+                            <input
+                                type="text"
+                                name="email"
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>비밀번호</label>
+                            <input
+                                type="password"
+                                name="password"
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <button>로그인</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         );
