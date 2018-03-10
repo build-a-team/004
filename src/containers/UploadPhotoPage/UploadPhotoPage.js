a16521e66a52572aa9058efbf136c463fc6e9fd3
rase <rase@afidev.com>
2018-03-10 22:49:36 +0900

    UploadPhotoPage [Updated] 이미지 선택 후 state에 저장, 업로드하기 버튼 누르면 firebase와 통신하도록 변경

---
+++ src/containers/UploadPhotoPage/UploadPhotoPage.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import uuidv1 from "uuid/v1";
+
+import firebase from "config/firebase";
+import classNames from "classnames/bind";
+import styles from "./UploadPhotoPage.scss";
+const cx = classNames.bind(styles);
+
+class UploadPhotoPage extends Component {
+    state = {
+        downloadURL: "", // 파이어베이스에 업로드 된 URL
+        src: "" // 미리보기 화면에 이미지를 띄우기 위한 로컬 경로
+    };
+
+    // 업로드 이벤트
+    handlePreUpload = event => {
+        const file = event.target.files[0];
+
+        const { name } = file;
+        console.log(name);
+
+        const nextState = {};
+        const reader = new FileReader();
+        reader.onload = e => {
+            nextState.src = e.target.result;
+            this.setState(nextState);
+        };
+        reader.readAsDataURL(file);
+
+        this.setState({
+            file
+        });
+    };
+
+    handleUpload = () => {
+        const { file } = this.state;
+        const { name } = file;
+        console.log(name);
+        const storageRef = firebase.storage().ref();
+        const metadata = {
+            contentType: "image/jpeg"
+        };
+
+        const uploadTask = storageRef
+            .child(`${name}${uuidv1()}`)
+            .put(file, metadata);
+        console.log(uploadTask);
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on(
+            firebase.storage.TaskEvent.STATE_CHANGED,
+            snapshot => {
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                const progress =
+                    snapshot.bytesTransferred / snapshot.totalBytes * 100;
+                console.log("Upload is " + progress + "% done");
+                switch (snapshot.state) {
+                    case firebase.storage.TaskState.PAUSED: // or 'paused'
+                        console.log("Upload is paused");
+                        break;
+                    case firebase.storage.TaskState.RUNNING: // or 'running'
+                        console.log("Upload is running");
+                        break;
+                    default:
+                        console.log("Upload status");
+                }
+            },
+            () => {
+                // A full list of error codes is available at
+                // https://firebase.google.com/docs/storage/web/handle-errors
+
+                console.log("err");
+            },
+            () => {
+                // Upload completed successfully, now we can get the download URL
+                // 파이어베이스에 받아오는 url. 나중에 DB에 넘겨줄 예정
+                const downloadURL = uploadTask.snapshot.downloadURL;
+                console.log("Success!");
+                console.log(downloadURL);
+                this.setState({
+                    downloadURL
+                });
+            }
+        );
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <input type="file" onChange={this.handlePreUpload} />
+                <br />
+                <img
+                    src={this.state.src}
+                    alt="미리보기"
+                    height="300"
+                    width="400"
+                />
+                <button onClick={this.handleUpload}>업로드하기</button>
+            </div>
+        );
+    }
+}
+
+export default UploadPhotoPage;
