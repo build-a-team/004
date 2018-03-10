69b99f8620cfbc9352de3e6b9cd089e7fa470a8a
LimHoSung <wonder789@naver.com>
2018-03-10 19:51:38 +0900

    디렉토리 구조 및 스타일 셋팅

---
+++ src/containers/LoginPage/LoginPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import classNames from "classnames/bind";
+import styles from "LoginPage.scss";
+const cx = classNames.bind(styles);
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <div classNames={cx("login-page")}>
+                <div />
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
