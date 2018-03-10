202e74139669bc7d4f72d1ddda2b8e979ac985e6
LimHoSung <wonder789@naver.com>
2018-03-11 03:17:46 +0900

    feed component추가

---
+++ src/containers/FeedPage/Feed/Feed.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import classNames from "classnames/bind";
+import styles from "./Feed.scss";
+const cx = classNames.bind(styles);
+
+const feed = props => {
+    return (
+        <div className={cx("feed")}>
+            <div className={cx("feed-img-wrap")}>
+                <img src={props.img} />
+            </div>
+            <div className={cx("feed-body")}>
+                <h3 className={cx("hash-tag")}>{props.hashtag}</h3>
+                <span className={cx("feed-count")}>{props.count}</span>
+            </div>
+        </div>
+    );
+};
+
+export default feed;
--- src/containers/FeedPage/FeedPage.js
+++ src/containers/FeedPage/FeedPage.js
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
+import Feed from "./Feed";
 import firebase from "config/firebase";
 import classNames from "classnames/bind";
 import styles from "./FeedPage.scss";
 import BottomNav from "components/BottomNav";
+import testImg from "assets/images/img-card.png";
 const cx = classNames.bind(styles);
-
 
 class FeedPage extends Component {
     render() {
         return (
             <div className={cx("feed-page")}>
                 <nav className={cx("feed-nav")}>
-            		<button className="col-4 hash-tag">#해시태그</button>
-            		<button className="col-4 hash-tag">#해시태그</button>
-            		<button className="col-4 plus">+</button>
-            	</nav>
+                    <button className="col-4 hash-tag">#해시태그</button>
+                    <button className="col-4 hash-tag">#해시태그</button>
+                    <button className="col-4 plus">+</button>
+                </nav>
                 <div className="container-tagImg">
-                    <div className="col-12 tag-Img">#데이트룩</div>
-                    <div className="col-12 tag-Img">#데이트룩</div>
-                    <div className="col-12 tag-Img">#데이트룩</div>
+                    <Feed img={testImg} hashtag="#Date_look" count="1000" />
+                    <Feed img={testImg} hashtag="#Girl_crush" count="355,000" />
+                    <Feed img={testImg} hashtag="#rain" count="2000" />
                 </div>
-                <BottomNav className="bottom-nav"/>          
+                <BottomNav className="bottom-nav" />
             </div>
         );
     }
