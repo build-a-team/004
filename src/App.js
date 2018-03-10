d84dab32229d27a7fa206fdbbe6c1915eea5f1ef
LimHoSung <wonder789@naver.com>
2018-03-11 04:19:44 +0900

    merge app

--- src/App.js
+++ src/App.js
@@ -4,30 +4,26 @@ import MainPage from "containers/MainPage";
 import SignupPage from "containers/SignupPage";
 import VoteResult from "containers/VoteResult";
 import FeedPage from "containers/FeedPage";
-<<<<<<< HEAD
 import UploadPhotoPage from "containers/UploadPhotoPage";
-=======
 import MyPage from "containers/MyPage";
->>>>>>> my page style
+
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import classNames from "classnames/bind";
 import styles from "./App.scss";
-import Cards from 'components/SwipeCard/Cards';
-import Card from 'components/SwipeCard/CardSwitcher';
-import { log } from 'ruucm-util';
+import Cards from "components/SwipeCard/Cards";
+import Card from "components/SwipeCard/CardSwitcher";
+import { log } from "ruucm-util";
 
 const cx = classNames.bind(styles);
 
-const data = ['Alexandre', 'Thomas', 'Lucien']
-const CustomAlertLeft = () => <span>Nop</span>
-const CustomAlertRight = () => <span>Ok</span>
+const data = ["Alexandre", "Thomas", "Lucien"];
+const CustomAlertLeft = () => <span>Nop</span>;
+const CustomAlertRight = () => <span>Ok</span>;
 class App extends Component {
-    
     render() {
         return (
             <BrowserRouter>
                 <div className={cx("app")}>
-
                     <Switch>
                         <Route path="/" exact component={MainPage} />
                         <Route path="/login" component={LoginPage} />
