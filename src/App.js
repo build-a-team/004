c080436f6312b4c42b1c30620f0288afeec44e4b
rase <rase@afidev.com>
2018-03-11 02:39:35 +0900

    App.js [Updated] Upload 라우터 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import LoginPage from "containers/LoginPage/LoginPage";
 import MainPage from "containers/MainPage/MainPage";
 import SignupPage from "containers/SignupPage/SignupPage";
+import UploadPhotoPage from "containers/UploadPhotoPage";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import classNames from "classnames/bind";
 import styles from "./App.scss";
@@ -17,6 +18,7 @@ class App extends Component {
                         <Route path="/" exact component={MainPage} />
                         <Route path="/login" component={LoginPage} />
                         <Route path="/signup" component={SignupPage} />
+                        <Route path="/upload" component={UploadPhotoPage} />
                         {/* Route Page */}
                     </Switch>
                 </div>
