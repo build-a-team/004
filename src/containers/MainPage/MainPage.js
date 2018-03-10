de73ffc95fe32ea4ee3ed7cc1e86e7e5d095d4a2
ruucm <ruucm@MacBook-Pro-5.local>
2018-03-11 04:13:59 +0900

    add swipe successfully

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -3,9 +3,24 @@ import { Link } from 'react-router-dom';
 import styles from "./MainPage.scss";
 import classNames from "classnames/bind";
 import BottomNav from "components/BottomNav";
+import Cards from 'components/SwipeCard/Cards';
+import Card from 'components/SwipeCard/CardSwitcher';
+import { log } from 'ruucm-util';
+
+
 const cx = classNames.bind(styles);
 
+
+const data = ['Alexandre', 'Thomas', 'Lucien']
+const CustomAlertLeft = () => {
+	return <span>Nop</span>
+}
+const CustomAlertRight = () => <span>Ok</span>
+
 class MainPage extends Component {
+	action(name) {
+        console.log(name);
+    }
     render() {
         return (
             <div className={cx("main-page")}>
@@ -14,7 +29,23 @@ class MainPage extends Component {
             		<button className="hash-tag">#해시태그</button>
             		<button className="show-result"><Link to="/vote-result">결과보기</Link></button>
             	</nav>
-                <div className="img">이미지</div>
+                <div>
+				  <h1>react swipe card</h1>
+				  <Cards
+				    alertRight={<CustomAlertRight />} 
+				    alertLeft={<CustomAlertLeft />} 
+				    onEnd={this.action('end')}
+				    className='master-root'>
+				    {data.map((item, key) => 
+				      <Card
+				          key={key}
+				          onSwipeLeft={()=>{this.action('swipe left!!!!!')}}
+				          onSwipeRight={()=>{this.action('swipe right!!!!')}}>
+				        <h2>{item}</h2>
+				      </Card>
+				    )}
+				  </Cards>
+				</div>
                 <div className="row select-btns-wrapper">
 				  <div className="col-6">
 				  	<button>스투핏</button>
