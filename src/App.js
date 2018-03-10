f9f160878accae04c287bfc65a0330bf858fcc8e
LimHoSung <wonder789@naver.com>
2018-03-10 19:25:38 +0900

    firebase config add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-  render() {
-    return <div className="App" />;
-  }
+    render() {
+        return <div className="App" />;
+    }
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+var config = {
+    apiKey: "AIzaSyBYn3hEaab7_3nuN4pB9KM111QQCSyobRE",
+    authDomain: "build004-f298c.firebaseapp.com",
+    databaseURL: "https://build004-f298c.firebaseio.com",
+    projectId: "build004-f298c",
+    storageBucket: "build004-f298c.appspot.com",
+    messagingSenderId: "629327571240"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
